7b4169ddc897c50de5205e8d72743081b39d3fad
ProjInt <projint200@gmail.com>
2022-12-01 19:27:04 -0300

    Alterado o Allowed Host

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7rv1#%#3s(j)k6x8+p5u_j8_4dx^m7_4t=db0=+=#93hrc1f5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://belasartesapi.herokuapp.com/']  #['https://projinteg3-drf-apba.herokuapp.com/']
+ALLOWED_HOSTS = ['*']  #['https://projinteg3-drf-apba.herokuapp.com/']
 
 
 # Application definition
