32e2a0b33a804f24b9a79c67d0fa366544a7ce6f
ProjInt <projint200@gmail.com>
2022-12-03 14:49:23 -0300

    Debug True

--- api/settings.py
+++ api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7rv1#%#3s(j)k6x8+p5u_j8_4dx^m7_4t=db0=+=#93hrc1f5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
